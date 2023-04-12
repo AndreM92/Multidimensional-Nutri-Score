efabb2f7ef42e435414b1167a9e0232e5a08746f
André Muth <andre.muth@tutanota.com>
2023-04-12 11:32:54 +0200

    creating the first functions and a DataFrame

--- NutrientScraper.py
+++ NutrientScraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.common.exceptions import *
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,88 +16,101 @@ import pandas as pd
 # Defining the startpage of Nährwertrechner.de
 startpage = r'https://www.naehrwertrechner.de/'
 
-# Driver setup
+# Driver and website setup
 driver = webdriver.Chrome(r"C:\Users\andre\Documents\Python\chromedriver.exe")
 driver.maximize_window()
 driver.get(startpage)
+time.sleep(3)
+
+# Cookie banner
+if driver.find_element('xpath', '/html/body/iframe[1]'):
+    driver.find_element(By.CLASS_NAME, 'fc-button-label').click()
 
 ########################################################################################################################
 # List of foods
-foods = ['Banane','Bitterschokolade','Rinderleber']
+foods = ['Banane','Broccoli','Ei','Rinderleber','Walnuss']
 # I standardize the information to 100g per food item
 quantity = '?menge=100'
 
-# searchbox
-se_xp = '//*[@id="one"]/div/div/div[1]/form/div/div[1]/input'
-WebDriverWait(driver,5).until(EC.presence_of_element_located(('xpath',se_xp)))
-searchbox = driver.find_element('xpath',se_xp)
-
-# Inspection of a specific food
-searchbox.click()
-searchbox.send_keys(Keys.BACKSPACE)
-searchbox.send_keys(foods[0])
-searchbox.send_keys(Keys.ENTER)
-# examination of the search results
-elements = driver.find_elements(By.CLASS_NAME,'box')
-print(elements)
+# searching process
+def searchFood(food):
+    driver.get(startpage)
+    se_xp = '//*[@id="one"]/div/div/div[1]/form/div/div[1]/input'
+    WebDriverWait(driver,5).until(EC.presence_of_element_located(('xpath',se_xp)))
+    searchbox = driver.find_element('xpath',se_xp)
+    searchbox.click()
+    searchbox.send_keys(Keys.BACKSPACE)
+    searchbox.send_keys(food)
+    searchbox.send_keys(Keys.ENTER)
+    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'box')))
+    elements = driver.find_elements(By.CLASS_NAME,'box')
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    links = [l['href'] for l in soup.find_all('a',href=True) if food.lower() in l['href'].lower()]
+    if len(links) >= 1:
+        # inspect the first entry
+        link = startpage + links[0] + quantity
+        driver.get(link)
 
 food = foods[0]
-soup = BeautifulSoup(driver.page_source,'html.parser')
-links = [l['href'] for l in soup.find_all('a',href=True) if food in l['href']]
-print(links)
+searchFood(food)
 
+# make sure that the page is completely open
+def openpage():
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located(('xpath','//*[@id="main"]/div/header/h2')))
+    name = driver.find_element('xpath','//*[@id="main"]/div/header/h2').text
+    openpage = driver.find_elements(By.CLASS_NAME,'tbl-read-more-btn')
+    if openpage:
+        try:
+            openpage[0].click()
+        except:
+            driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            openpage[0].click()
+        # scroll down
+        driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    return name
 
-# inspect the selected entry
-link = startpage + links[0] + quantity
-driver.get(link)
-# scroll down and open the full page
-driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-driver.find_element(By.CLASS_NAME,'tbl-read-more-btn').click()
+name = openpage()
 
-# Scrape the page
-soup = BeautifulSoup(driver.page_source,'html.parser')
-tables = soup.find_all('table',class_='alt nwdetails')
-#print(len(tables))
+# scrape the data
+def getData(food,name):
+    data = []
+    header,columns = ['' for i in range(2)]
 
-rows = tables[2].find_all('tr')
-f_rows = [r for r in rows if not 'Tagesbedarf' in r.text]
+    time.sleep(1)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    tables = soup.find_all('table',class_='alt nwdetails')
+    if len(tables) >= 1:
+        rows = tables[2].find_all('tr')
+        f_rows = [r for r in rows if not 'Tagesbedarf' in r.text]
+        columns = [[c.text for c in r.find_all('td')[:2]] for r in f_rows]
+        f_columns = [e for e in columns if len(e) > 1]
 
-columns = [[c.text for c in r.find_all('td')[:2]] for r in f_rows]
-f_columns = [e for e in columns if len(e) > 1]
+    for t in tables:
+        rows = t.find_all('tr')
+        f_rows = [r for r in rows if not 'Tagesbedarf' in r.text and not 'Richtwert' in r.text]
+        columns = [[c.text.strip() for c in r.find_all('td')[:2]] for r in f_rows]
+        f_columns = [e for e in columns if len(e) > 1]
+        data.extend(f_columns)
 
-data = []
-for t in tables:
-    rows = t.find_all('tr')
-    f_rows = [r for r in rows if not 'Tagesbedarf' in r.text and not 'Richtwert' in r.text]
-    columns = [[c.text.strip() for c in r.find_all('td')[:2]] for r in f_rows]
-    f_columns = [e for e in columns if len(e) > 1]
-    data.extend(f_columns)
+    return data
+
+data = getData(food,name)
 
 for d in data:
     print(d)
 # print(data)
 
+# create a DataFrame
+header = ['food'] + [d[0] for d in data]
+amount = [name] + [d[1] for d in data]
+# Empty DataFrame
+df = pd.DataFrame(columns=header)
+# As long as I only have one or a few lines of data, I'm using this command:
+df.loc[len(df)] = amount
+print(df)
+
 
 # Notes
 ########################################################################################################################
-for r in f_rows:
-    columns = [n.text.strip() if len(n.text.strip()) > 1 for n in r.find_all('td')[:2]]
-    for c in columns:
-        print(len(c))
-    print(columns)
-    nutrient, amount = [n.text.strip() for n in c.find_all('td')[:2]]
-    print(nutrient)
-    print(amount)
 
-data = []
-header = []
-amount = []
-for t in tables[1:3]:
-    rows = t.find_all('tr')
-    f_rows = [r for r in rows if not 'Tagesbedarf' in r.text]
-    for r in rows:
-        name, quant = [e.text.strip() for e in r.find_all('td')[:2]]
-        header.extend(name)
-        amount.extend(quant)
-df = pd.DataFrame(columns = header, rows = amount)
-
+#df = pd.DataFrame(amount,columns=header)
