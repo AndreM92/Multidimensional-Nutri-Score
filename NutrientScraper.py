56865e20ee06c3fd119fb16851e820fba0d46f92
André Muth <andre.muth@tutanota.com>
2023-04-13 15:12:42 +0200

    foodloop, DataFrame and debugging

--- NutrientScraper.py
+++ NutrientScraper.py
@@ -28,11 +28,13 @@ if driver.find_element('xpath', '/html/body/iframe[1]'):
 
 ########################################################################################################################
 # List of foods
-foods = ['Banane','Broccoli','Ei','Rinderleber','Walnuss']
+foods = ['Banane','Broccoli','Ei']
+#'Rinderleber','Walnuss'
+
 # I standardize the information to 100g per food item
 quantity = '?menge=100'
 
-# searching process
+# Searching process
 def searchFood(food):
     driver.get(startpage)
     se_xp = '//*[@id="one"]/div/div/div[1]/form/div/div[1]/input'
@@ -43,7 +45,6 @@ def searchFood(food):
     searchbox.send_keys(food)
     searchbox.send_keys(Keys.ENTER)
     WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'box')))
-    elements = driver.find_elements(By.CLASS_NAME,'box')
     soup = BeautifulSoup(driver.page_source,'html.parser')
     links = [l['href'] for l in soup.find_all('a',href=True) if food.lower() in l['href'].lower()]
     if len(links) >= 1:
@@ -51,54 +52,132 @@ def searchFood(food):
         link = startpage + links[0] + quantity
         driver.get(link)
 
-food = foods[0]
-searchFood(food)
+# food = foods[0]
+# searchFood(food)
 
-# make sure that the page is completely open
-def openpage():
+# Make sure that the page is completely open
+def openPage():
+    name = ''
     WebDriverWait(driver, 5).until(EC.presence_of_element_located(('xpath','//*[@id="main"]/div/header/h2')))
+    # additional sleeptime
+    time.sleep(1)
     name = driver.find_element('xpath','//*[@id="main"]/div/header/h2').text
-    openpage = driver.find_elements(By.CLASS_NAME,'tbl-read-more-btn')
+    openpage = driver.find_element('xpath', '//a[contains(text(), "Weiterlesen")]')
+    scrheight = driver.execute_script('return document.body.scrollHeight')
     if openpage:
         try:
-            openpage[0].click()
+            openpage.click()
         except:
             driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            openpage[0].click()
+            try:
+                openpage.click()
+            except:
+                pass
+        if scrheight == driver.execute_script('return document.body.scrollHeight'):
+            time.sleep(1)
+            try:
+                openpage.click()
+            except:
+                try:
+                    driver.find_elements(By.CLASS_NAME, 'tbl-read-more-btn').click()
+                except:
+                    pass
         # scroll down
         driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+
     return name
 
-name = openpage()
+# name = openPage()
+
 
 # scrape the data
-def getData(food,name):
-    data = []
+def getData(food):
+    data = [['food', food]]
     header,columns = ['' for i in range(2)]
-
     time.sleep(1)
     soup = BeautifulSoup(driver.page_source,'html.parser')
     tables = soup.find_all('table',class_='alt nwdetails')
     if len(tables) >= 1:
-        rows = tables[2].find_all('tr')
-        f_rows = [r for r in rows if not 'Tagesbedarf' in r.text]
-        columns = [[c.text for c in r.find_all('td')[:2]] for r in f_rows]
-        f_columns = [e for e in columns if len(e) > 1]
-
-    for t in tables:
-        rows = t.find_all('tr')
-        f_rows = [r for r in rows if not 'Tagesbedarf' in r.text and not 'Richtwert' in r.text]
-        columns = [[c.text.strip() for c in r.find_all('td')[:2]] for r in f_rows]
-        f_columns = [e for e in columns if len(e) > 1]
-        data.extend(f_columns)
+        for t in tables:
+            rows = t.find_all('tr')
+            f_rows = [r for r in rows if not 'Tagesbedarf' in r.text and not 'Richtwert' in r.text]
+            columns = [[c.text.strip() for c in r.find_all('td')[:2]] for r in f_rows]
+            f_columns = [e for e in columns if len(e) > 1]
+            data.extend(f_columns)
 
     return data
 
-data = getData(food,name)
+# ftable = getData(food)
+# for t in ftable:
+#     print(t)
+# print(ftable)
 
-for d in data:
-    print(d)
-# print(data)
+
+# A loop to scrape the nutrients for every food in the list
+foodDict = {}
+header = ''
+
+for food in foods:
+    foodID = foods.index(food)
+    searchFood(food)
+    name = openPage()
+    nutrients = getData(food)
+    if header == '':
+        header = [d[0] for d in nutrients[1:]]
+    if len(foodDict) == 0:
+        foodDict = {'ID':[], 'food':[], 'entry':[]}
+        for h in header:
+            foodDict[h] = []
+    foodDict['ID'].append(foodID)
+    foodDict['food'].append(nutrients[0][1])
+    foodDict['entry'].append(name)
+    for f in foodDict:
+        if f in header:
+            col = header.index(f)
+            foodDict[f].append(nutrients[col][1])
+
+# print(foodDict)
+# for f in foodDict:
+#     print(foodDict[f])
+
+
+# Transform the Dictionary to a DataFrame
+df = pd.DataFrame(foodDict)
+print(df)
+
+########################################################################################################################
+# Notes
+
+# Test
+nutrients = [['A',1],['B',5],['E',3]]
+
+# A loop to scrape the nutrients for every food in the list
+name = ['n']
+header = ''
+foodDict = {}
+
+for food in foods:
+    foodID = foods.index(food)
+#    searchFood(food)
+#    name = openpage()
+#    nutrients = getData(food,name)
+    if header == '':
+        header = [d[0] for d in nutrients]
+    if len(foodDict) == 0:
+        foodDict = {'ID':[], 'food':[], 'entry':[]}
+        for h in header:
+            foodDict[h] = []
+    foodDict['ID'].append(foodID)
+    foodDict['food'].append(food)
+    foodDict['entry'].append(name)
+    for h in header:
+        col = header.index(h)
+        if nutrients[col][0] in h:
+            foodDict[h].append(nutrients[col][1])
+        else:
+            foodDict[h].append('NaN')
+    nutrients = [['A', 1], ['D', 5], ['E', 3]]
+print(foodDict)
 
 # create a DataFrame
 header = ['food'] + [d[0] for d in data]
@@ -109,8 +188,4 @@ df = pd.DataFrame(columns=header)
 df.loc[len(df)] = amount
 print(df)
 
-
-# Notes
-########################################################################################################################
-
-#df = pd.DataFrame(amount,columns=header)
+#df = pd.DataFrame(data,columns=header)
